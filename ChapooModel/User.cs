b005464263eca729fa4a82cf06466bd4ebfc3564
DatIsHetLeven <muratcelem187@gmail.com>
2021-04-13 00:43:48 +0200

    Changed consistency  -> All in English.

---
+++ ChapooModel/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChapooModel
+{
+    public class User
+    {
+        public int userId { get; set; }
+        public string userName { get; set; }
+        public string userPassword { get; set; }
+        public int userCodr { get; set; }
+    }
+}
