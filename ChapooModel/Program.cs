d0e4eba8468975b1445fe1fcde209de839de80b7
DatIsHetLeven <muratcelem187@gmail.com>
2021-04-12 21:22:01 +0200

    Made the 3 layer template

--- ChapooModel/Program.cs
+++ ChapooModel/Program.cs
@@ -4,9 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ChapooDAL
+namespace ChapooModel
 {
-    public class Class1
+    class Program
     {
+        static void Main(string[] args)
+        {
+        }
     }
 }
