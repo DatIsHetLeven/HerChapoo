275b1a4cfc4b302133c112e976bd1237d939fd87
DatIsHetLeven <muratcelem187@gmail.com>
2021-04-20 14:22:31 +0200

    Revert "Merge branch 'master' of https://github.com/DatIsHetLeven/HerChapoo"
    This reverts commit 6d2b9a6c3800b45317007a02db803086647b0232, reversing
    changes made to cba2b50c722d5a6dcd13b1d28e10b277b9ee8f44.

--- ChapooUI/login.cs
+++ ChapooUI/login.cs
@@ -25,8 +25,8 @@ namespace ChapooUI
         private void btn_Inloggen_Click(object sender, EventArgs e)
         {
             this.Hide();
-            Dashboard dashboardView = new Dashboard();
-            dashboardView.ShowDialog();
+            AdminAllUsers adminAll = new AdminAllUsers();
+            adminAll.ShowDialog();
             this.Close();
 
             //Check if username and password are not empty.
@@ -38,9 +38,22 @@ namespace ChapooUI
                 //If user exist
                 if (user.userName != "")
                 {
+                    //If user is Admin
+                    if (user.userCode == 3)
+                    {
                         this.Hide();
+                        AdminDashboard adminDashboard = new AdminDashboard();
+                        adminDashboard.ShowDialog();
+                        this.Close();
+                    }
+                    //If user is not an Admin
+                    else
+                    {
+                        this.Hide();
+                        Dashboard dashboardView = new Dashboard();
                         dashboardView.ShowDialog();
                         this.Close();
+                    }
                 }
                 else
                 {
